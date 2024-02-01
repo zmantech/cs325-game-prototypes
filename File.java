df9b6e36a4673f63201340baff566a0f5622754f
zmantech <zmantech13@gmail.com>
2024-02-01 16:08:30 -0500

    added file

---
+++ File.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*; 
+
+public class File {
+    //this class will handle reading files from temp which 
+    //should all be in a sub directory
+    //GUI others can call a function to get the weight etc
+    //GUI others can call a function to change the weight etc in file
+
+}
